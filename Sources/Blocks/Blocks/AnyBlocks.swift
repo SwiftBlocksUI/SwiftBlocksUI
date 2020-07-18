10e7042d6b179a4370ca676a7d1e46472b873492
Helge Heß <devteam@zeezide.de>
2020-07-18 16:05:34 +0200

    Add docs to the `AnyBlocks`
    And cleanup the implementation a little. Also:
    add a blocks builder init.

--- Sources/Blocks/Blocks/AnyBlocks.swift
+++ Sources/Blocks/Blocks/AnyBlocks.swift
@@ -6,23 +6,53 @@
 //  Copyright © 2020 ZeeZide GmbH. All rights reserved.
 //
 
-public struct AnyBlocks: Blocks, BlocksPrimitive {
+/**
+ * A type eraser for Blocks.
+ *
+ * Can be used to mix & match Blocks of different static types.
+ *
+ * Example:
+ *
+ *     var activeBlocks : AnyBlocks {
+ *       switch status {
+ *         case "order" : return AnyBlocks(OrderStatus())
+ *         case "home"  : return AnyBlocks(HomePage())
+ *       }
+ *     }
+ *     var body: some View {
+ *       Section {
+ *         activeBlocks
+ *       }
+ *     }
+ */
+public struct AnyBlocks: Blocks {
   
   public typealias Body = Never
   
-  private let erasedRender : ( BlocksContext ) throws -> Void
+  fileprivate let erasedRender : ( BlocksContext ) throws -> Void
   
   public init<B: Blocks>(_ blocks: B) {
     self.erasedRender = { context in
       try context.render(blocks)
     }
   }
+  
+  public init<B: Blocks>(@BlocksBuilder content: () -> B) {
+    let blocks = content()
+    self.erasedRender = { context in
+      try context.render(blocks)
+    }
+  }
+  
   public init<B: Blocks & BlocksPrimitive>(_ blocks: B) {
     self.erasedRender = { context in
       try blocks.render(in: context)
     }
   }
+}
 
+extension AnyBlocks: BlocksPrimitive {
+  
   public func render(in context: BlocksContext) throws {
     try erasedRender(context)
   }
