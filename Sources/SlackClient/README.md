dd7613e2330bb31f624e83fe8fc988fa6da0a112
Helge Heß <devteam@zeezide.de>
2020-11-16 13:50:52 +0100

    Reorg client extensions
    Some code cleanup.

--- Sources/SlackClient/README.md
+++ Sources/SlackClient/README.md
@@ -3,3 +3,22 @@
 A tinsy client for Slack. 
 This is only intended to implement the pieces necessary for SwiftBlocksUI,
 not as a general purpose package.
+
+The required token (`xoxp-xyz...`) can either be provided using the
+`token` parameter, or the `SLACK_ACCESS_TOKEN` variable can be set
+in the environment.
+
+
+### SwiftBlocksUI
+
+Note: The `SwiftBlocksUI` module contains some extensions to this basic client.
+
+Example:
+
+```swift
+let client = SlackClient() // use SLACK_ACCESS_TOKEN env var
+
+client.views.open(MyView(), with: action.triggerID) { error, json in
+   ...
+}
+```
---
+++ Sources/SwiftBlocksUI/Client/OpenModal.swift
@@ -0,0 +1,54 @@
+//
+//  OpenModal.swift
+//  SwiftBlocksUI
+//
+//  Created by Helge Heß.
+//  Copyright © 2020 ZeeZide GmbH. All rights reserved.
+//
+
+import struct SlackClient.SlackClient
+import enum   SlackBlocksModel.Block
+import struct SlackBlocksModel.View
+import enum   SlackBlocksModel.InteractiveRequest
+
+public extension SlackClient.Views {
+
+  fileprivate struct CouldNotProcessView: Swift.Error {} // FIXME
+
+  // TODO: make it a BlocksBuilder, but figure out how to do the yield.
+  // TODO: replace arguments w/ ViewState in Context
+  // All this probably belongs into the specific endpoint?
+  
+  func open<V>(_ view: V, with triggerID: SlackBlocksModel.TriggerID,
+               yield: @escaping (SlackClient.APIError?, [String : Any]) -> Void)
+       where V: Blocks
+  {
+    var apiView : SlackBlocksModel.View
+    
+    do {
+      let context = BlocksContext()
+      context.surface = .modal
+      
+      try context.render(view)
+
+      if context.view == nil {
+        context.log.warning("no explicit view passed to views.open \(view)")
+      }
+      context.finishView(defaultTitle: "\(type(of: view))")
+
+      guard let ctxView = context.view else {
+        assertionFailure("missing view even after finish ... \(context)")
+        return yield(SlackClient.APIError.noValidJSONResponseContent(nil), [:])
+      }
+      apiView = ctxView
+    }
+    catch {
+      //log.error("Failed to render blocks: \(blocks)\n  error: \(error)")
+      //return sendStatus(500)
+      assertionFailure("error: \(error)")
+      return yield(SlackClient.APIError.noValidJSONResponseContent(nil), [:])
+    }
+    
+    self.open(apiView, with: triggerID, yield: yield)
+  }
+}
