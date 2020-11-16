3c4e01aeaca89969042cccffb647f33bb5c36857
Helge Heß <devteam@zeezide.de>
2020-11-16 13:19:32 +0100

    Add a Markdown Blocks
    Directly render arbitrary markdown. Doesn't work
    everywhere yet (e.g. not within a Quote {} blocks).

--- Sources/Blocks/Blocks/Elements/Markdown.swift
+++ Sources/Blocks/Blocks/Elements/Markdown.swift
@@ -1,8 +1,60 @@
 //
-//  File.swift
-//  
+//  Markdown.swift
+//  Markdown
 //
-//  Created by Helge Heß on 16.11.20.
+//  Created by Helge Heß.
+//  Copyright © 2020 ZeeZide GmbH. All rights reserved.
 //
 
-import Foundation
+import class Foundation.Formatter
+import class Foundation.Bundle
+import enum  SlackBlocksModel.Block
+
+public struct Markdown: Equatable {
+  
+  // TODO: Doesn't work in quote yet
+  
+  public let markdown : String
+  
+  @inlinable
+  public init(_ markdown: String) {
+    self.markdown = markdown
+  }
+  
+  @inlinable
+  static func +(lhs: Markdown, rhs: Markdown) -> Markdown {
+    // FIXME: Make smarter, combine runs
+    return Markdown(lhs.markdown + rhs.markdown)
+  }
+  @inlinable
+  static func +(lhs: Markdown, rhs: String) -> Markdown {
+    return Markdown(lhs.markdown + rhs)
+  }
+}
+
+extension Markdown: Blocks {
+  public typealias Body = Never
+}
+
+public extension Markdown {
+  
+  @usableFromInline
+  internal func adding(_ modifier: Text.Modifier) -> Markdown {
+    return Markdown(modifier.markdownStyle(markdown))
+  }
+  @inlinable func bold()   -> Markdown { return adding(.bold)   }
+  @inlinable func italic() -> Markdown { return adding(.italic) }
+  @inlinable func code()   -> Markdown { return adding(.code)   }
+  @inlinable func strike() -> Markdown { return adding(.strike) }
+}
+
+public extension Markdown {
+  
+  var slackMarkdownString: String {
+    return markdown
+  }
+  
+  var contentString: String {
+    return markdown // FIXME: remove markdown
+  }
+}
