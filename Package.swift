a46be04218a208a142a5d72f67abdc499d21d0e2
Helge Heß <me@helgehess.eu>
2020-12-16 23:04:30 +0100

    Bump requirement to Macro 0.5.8
    The `http.request` is required in the client now.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   
   dependencies: [
     .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.5.7"),
+             from: "0.5.8"),
     .package(url: "https://github.com/Macro-swift/MacroExpress.git",
              from: "0.5.7"),
     .package(url: "https://github.com/Macro-swift/MacroApp.git",
