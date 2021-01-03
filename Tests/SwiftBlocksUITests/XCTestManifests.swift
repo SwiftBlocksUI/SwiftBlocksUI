d928271798bd41a020da9b6e740ae34df2e818aa
Helge Heß <me@helgehess.eu>
2021-01-03 21:49:33 +0100

    Fix Linux test setup
    ... incorrect ID.

--- Tests/SwiftBlocksUITests/XCTestManifests.swift
+++ Tests/SwiftBlocksUITests/XCTestManifests.swift
@@ -3,9 +3,9 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [ XCTestCaseEntry ] {
   return [
-    testCase(SwiftBlocksUI.allTests),
-    testCase(BlockIDTests .allTests),
-    testCase(PickerTests  .allTests)
+    testCase(SwiftBlocksUITests.allTests),
+    testCase(BlockIDTests      .allTests),
+    testCase(PickerTests       .allTests)
   ]
 }
 #endif
