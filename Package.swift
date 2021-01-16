040b8ac6806c429e20efd9093d71fe259f18bcc7
Helge Heß <devteam@zeezide.de>
2021-01-16 17:53:17 +0100

    Bump Macro version to 0.8.0
    Required for some new exports.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   
   dependencies: [
     .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.6.3"),
+             from: "0.8.0"),
     .package(url: "https://github.com/Macro-swift/MacroExpress.git",
              from: "0.6.1"),
     .package(url: "https://github.com/Macro-swift/MacroApp.git",
